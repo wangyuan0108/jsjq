7d6a5fc0adfd49cd725ab2d604061eb63fe36ad7
wangyuan0108 <18626869772@163.com>
2017-02-20 14:11:22 +0800

    资料

---
+++ vue/vue练习第二发-购物车/js/data.js
@@ -0,0 +1,23 @@
+/**
+ * Created by LF on 2017/2/13.
+ */
+var data= [
+    {
+        name:'iPhone 6',
+        price:5499,
+        id:1,
+        count:1
+    },
+    {
+        name:'iPad',
+        price:3299,
+        id:2,
+        count:1
+    },
+    {
+        name:'iMac',
+        price:15999,
+        id:3,
+        count:1
+    }
+];
